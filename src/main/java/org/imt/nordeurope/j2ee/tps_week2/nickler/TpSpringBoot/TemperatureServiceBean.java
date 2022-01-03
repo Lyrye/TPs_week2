cbee2083c6caf36a462a0702dfa5aa89bcb5b104
Lila Nickler <lila.nickler@gmail.com>
2022-01-03 14:41:55 +0100

    New package for beans

--- src/main/java/org/imt/nordeurope/j2ee/tps_week2/nickler/TpSpringBoot/TemperatureServiceBean.java
+++
@@ -1,7 +0,0 @@
-package org.imt.nordeurope.j2ee.tps_week2.nickler.TpSpringBoot;
-
-import java.math.BigDecimal;
-
-public interface TemperatureServiceBean {
-    BigDecimal getTemperature(String country);
-}
