ae99e2c15c4b1daa20f267b3ecfcf9479f3efd53
Lila Nickler <lila.nickler@gmail.com>
2022-01-04 10:44:17 +0100

    Servlet + index.jsp

--- src/main/java/org/imt/nordeurope/j2ee/tps_week2/nickler/TpSpring/servlets/TemperatureServlet.java
+++ src/main/java/org/imt/nordeurope/j2ee/tps_week2/nickler/TpSpring/servlets/TemperatureServlet.java
@@ -1,15 +1,20 @@
 package org.imt.nordeurope.j2ee.tps_week2.nickler.TpSpring.servlets;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+@WebServlet(name = "Temperature", value = "/Temperature")
 public class TemperatureServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+        if(request.getParameter("action").equals("GetTempartures")){
 
+        }
 
     }
 
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,28 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Lila
+  Date: 04/01/2022
+  Time: 10:37
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>INDEX - CDAW2</title>
+</head>
+<body>
+<h1>INDEX - CDAW2</h1>
+<h2>TP Spring</h2>
+
+<h3>TP</h3>
+<ul>
+
+</ul>
+
+<h2>Auteurs</h2>
+<p>Guillaume CEUNINCK, Lila NICKLER, Benjamin LEROUX</p>
+
+
+</body>
+</html>
